58a2cc21239c40215dd217031ceb5cd72cfa1d7a
BANCUNGUYE66 <bancunguye66@gmail.com>
2025-10-29 18:16:42 +0000

    Fix PEP8 validation errors for Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -18,12 +18,12 @@ if __name__ == "__main__":
         
         # Define the base URL for the API
         base_url = "https://jsonplaceholder.typicode.com"
-        
+
         try:
             # --- 1. Get User Information (Employee Name) ---
             # Construct the URL for user data
             user_url = "{}/users/{}".format(base_url, employee_id)
-            
+
             with urllib.request.urlopen(user_url) as response:
                 user_data_bytes = response.read()
                 user_data_str = user_data_bytes.decode('utf-8')
@@ -31,11 +31,11 @@ if __name__ == "__main__":
             
             # Safely get the employee's username
             username = user_data.get("username")
-            
+
             # --- 2. Get TODO List Information ---
             # Construct the URL for the user's TODO list
             todos_url = "{}/todos?userId={}".format(base_url, employee_id)
-            
+
             with urllib.request.urlopen(todos_url) as response:
                 todos_data_bytes = response.read()
                 todos_data_str = todos_data_bytes.decode('utf-8')
@@ -44,21 +44,21 @@ if __name__ == "__main__":
             # --- 3. Write Data to CSV File ---
             # The filename is the user ID
             filename = "{}.csv".format(employee_id)
-            
+
             # Open the file for writing
             # newline='' is the standard way to write CSVs in Python
             with open(filename, mode='w', newline='') as csv_file:
                 # Create a CSV writer
                 # We use quoting=csv.QUOTE_ALL to put quotes around every field
                 writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-                
+
                 # Loop through all tasks and write them to the file
                 for task in todos_data:
                     # Get the required data for each row
                     user_id = employee_id
                     task_completed = task.get("completed")
                     task_title = task.get("title")
-                    
+
                     # Write the row in the specified format
                     writer.writerow([user_id,
                                      username,
